7c677f1661fc3ddd87b3226a39c54db3c98b9a0f
tailow <tailows@outlook.com>
2024-12-11 22:52:00 +0200

    Added timer

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,16 @@
+use std::time::SystemTime;
+
 mod days;
 
 fn main() {
+    let start = SystemTime::now();
+
     days::day11::run();
+
+    match start.elapsed() {
+        Ok(elapsed) => {
+            println!("Time elapsed: {} ms", elapsed.as_millis());
+        }
+        _ => {}
+    }
 }
